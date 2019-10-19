530ce44a2a4616ca3bb395348971eedbe3e52295
Kostya Morenov <kostya.morenov@gmail.com>
2019-10-19 04:16:20 +0300

    added layout switch, load more, spinner

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,32 +1,51 @@
 import React from "react";
 
-const Posts = ({ posts }) => {
-  return (
-    <div className="uk-grid uk-child-width-1-2@s uk-child-width-1-2@m">
-      {posts.map(post => {
-        return (
-          <div key={post.id}>
-            <a
-              href=""
-              className="uk-card uk-card-default uk-margin-medium-bottom uk-child-width-1-2@s uk-grid-collapse uk-margin"
-              data-uk-grid
-            >
-              <div className="uk-card-media-left uk-cover-container">
-                <img src="https://picsum.photos/600/400" alt="" data-uk-cover />
-                <canvas width={600} height={400} />
-              </div>
-              <div>
-                <div className="uk-card-body">
-                  <h3 className="uk-card-title">{post.title}</h3>
-                  <p>{post.body}</p>
-                </div>
-              </div>
-            </a>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+const Posts = ({posts, layout}) => {
+    return (
+        <div className={layout == "list" ?
+            "uk-grid uk-child-width-1-2@s uk-child-width-1-2@m" : "uk-grid uk-child-width-1-2@s uk-child-width-1-3@m"}>
+            {posts.map(post => {
+                return (
+                    <div key={post.id}>
+
+                        {layout == 'list' ?
+                            <a
+                                href=""
+                                className="uk-card uk-card-default uk-margin-medium-bottom uk-child-width-1-2@s uk-grid-collapse uk-margin"
+                                data-uk-grid
+                            >
+                                <div className="uk-card-media-left uk-cover-container">
+                                    <img src="https://picsum.photos/600/400" alt="" data-uk-cover/>
+                                    <canvas width={600} height={400}/>
+                                </div>
+                                <div>
+                                    <div className="uk-card-body">
+                                        <h3 className="uk-card-title">{post.title}</h3>
+                                        <p>{post.body}</p>
+                                        <a href="#" className="uk-button uk-button-text">Read more</a>
+                                    </div>
+                                </div>
+                            </a>
+                            :
+                            <div>
+                                <div className="uk-card uk-card-default uk-margin-medium-bottom">
+                                    <div className="uk-card-header">
+                                        <h3 className="uk-card-title uk-margin-remove-bottom">{post.title}</h3>
+                                    </div>
+                                    <div className="uk-card-body">
+                                        <p>{post.body}</p>
+                                    </div>
+                                    <div className="uk-card-footer">
+                                        <a href="#" className="uk-button uk-button-text">Read more</a>
+                                    </div>
+                                </div>
+                            </div>
+                        }
+                    </div>
+                );
+            })};
+        </div>
+    )
+}
 
 export default Posts;
--- src/index.js
+++ src/index.js
@@ -8,95 +8,117 @@ import Pagination from "./components/Pagination";
 // https://tppr.me/nYtAv
 
 class App extends React.Component {
-  state = {
-    search: "",
-    order: "desc",
-    perPage: 6,
-    page: 1,
-    total: 100,
-    layout: "grid", // list
-    posts: []
-  };
+    state = {
+        search: "",
+        order: "desc",
+        perPage: 6,
+        page: 1,
+        total: 100,
+        layout: "grid", // list
+        posts: [],
+        loading: false
+    };
 
-  fetch = () => {
-    const params = `?_limit=${this.state.perPage}&_page=${
-      this.state.page
-    }&title_like=${this.state.search}&_sort=title&_order=${this.state.order}`;
+    fetch = () => {
+        const params = `?_limit=${this.state.perPage}&_page=${
+            this.state.page
+            }&title_like=${this.state.search}&_sort=title&_order=${this.state.order}`;
 
-    fetch(`https://jsonplaceholder.typicode.com/posts${params}`)
-      .then(response => response.json())
-      .then(posts => {
+        fetch(`https://jsonplaceholder.typicode.com/posts${params}`)
+            .then(response => response.json())
+            .then(posts => {
+                this.setState({
+                    posts
+                });
+            });
+    };
+
+    handlePage = page => {
+        this.setState(
+            {
+                page
+            },
+            () => {
+                this.fetch();
+            }
+        );
+    };
+
+    onChange = e => {
+        const value = e.target.value;
+        const name = e.target.name;
+
+        this.setState(
+            {
+                [name]: value
+            },
+            () => {
+                this.fetch();
+            }
+        );
+    };
+
+    handleLayout = value => {
         this.setState({
-          posts
+            layout: value
         });
-      });
-  };
+    };
 
-  handlePage = page => {
-    this.setState(
-      {
-        page
-      },
-      () => {
+    loadMore = () => {
+        this.setState({loading: true, perPage: this.state.perPage*2}, () => {
+            this.fetch()
+            this.setState({loading: false})
+        })
+
+    }
+
+    componentDidMount() {
         this.fetch();
-      }
-    );
-  };
+    }
 
-  onChange = e => {
-    const value = e.target.value;
-    const name = e.target.name;
-
-    this.setState(
-      {
-        [name]: value
-      },
-      () => {
-        this.fetch();
-      }
-    );
-  };
-
-  handleLayout = value => {
-    this.setState({
-      layout: value
-    });
-  };
-
-  componentDidMount() {
-    this.fetch();
-  }
-
-  render() {
-    return (
-      <main className="uk-main">
-        <div className="uk-section">
-          <div className="uk-container">
-            {/* <pre tab="2">{JSON.stringify(this.state)}</pre> */}
-            <FilterBar
-              layout={this.state.layout}
-              perPage={this.state.perPage}
-              order={this.state.order}
-              search={this.state.search}
-              handleSearch={this.handleSearch}
-              handleLayout={this.handleLayout}
-              handleOrder={this.handleOrder}
-              handlePerPage={this.handlePerPage}
-              onChange={this.onChange}
-            />
-            <Posts posts={this.state.posts} />
-            <Pagination
-              page={this.state.page}
-              total={this.state.total}
-              perPage={this.state.perPage}
-              handlePage={this.handlePage}
-            />
-          </div>
-        </div>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main className="uk-main">
+                <div className="uk-section">
+                    <div className="uk-container">
+                        {/* <pre tab="2">{JSON.stringify(this.state)}</pre> */}
+                        <FilterBar
+                            layout={this.state.layout}
+                            perPage={this.state.perPage}
+                            order={this.state.order}
+                            search={this.state.search}
+                            handleSearch={this.handleSearch}
+                            handleLayout={this.handleLayout}
+                            handleOrder={this.handleOrder}
+                            handlePerPage={this.handlePerPage}
+                            onChange={this.onChange}
+                        />
+                        <Posts
+                            posts={this.state.posts}
+                            layout={this.state.layout}
+                        />
+                        <div className="uk-margin">
+                            <button className="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom"
+                                    onClick={() => this.loadMore()}>
+                                {this.state.loading ?
+                                    <div className="uk-margin-small-left" uk-spinner="ratio: 0.6"></div>
+                                    :
+                                    'Load more'
+                                }
+                            </button>
+                        </div>
+                        <Pagination
+                            page={this.state.page}
+                            total={this.state.total}
+                            perPage={this.state.perPage}
+                            handlePage={this.handlePage}
+                        />
+                    </div>
+                </div>
+            </main>
+        );
+    }
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App/>, rootElement);
